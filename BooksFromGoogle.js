16acffff8b89f1bc71832123e73a3977b4aecf8e
Mark Levison <mark@agilepainrelief.com>
2023-02-08 17:01:00 -0500

    test the results from google books api

--- BooksFromGoogle.js
+++ BooksFromGoogle.js
@@ -4,7 +4,7 @@ const GOOGLE_BOOKS_TITLE_TERM = "intitle:"
 export async function queryGoogleBookAPI(title) {
   let encodedTitle = encodeURIComponent(GOOGLE_BOOKS_TITLE_TERM + title);
   let finalURL = GOOGLE_BOOKS_API_URL + "?q=" + encodedTitle + "&maxResults=10";
-  console.log(finalURL);
+  // console.log(finalURL);
   const response = await fetch(finalURL);
   const data = await response.json();
   return data;
--- BooksFromGoogle.test.js
+++ BooksFromGoogle.test.js
@@ -10,7 +10,9 @@ test('how ManyBooks', async () => {
     expect(numBooks).toBeLessThanOrEqual(10)
 });
 
-test('how ManyBooks', async () => {
-    const numBooks = await howManyBooks("Flowers for Algernon")
-    expect(numBooks).toBeLessThanOrEqual(10)
+test('first book details', async () => {
+    const bookDesc = await queryGoogleBookAPI("Flowers for Algernon");
+    expect(bookDesc.items[0].volumeInfo.title).toEqual("Flowers for Algernon");
+    expect(bookDesc.items[0].volumeInfo.authors[0]).toEqual("Daniel Keyes");
+    expect(bookDesc.items[0].volumeInfo.categories[0]).toEqual("Fiction");
 });
