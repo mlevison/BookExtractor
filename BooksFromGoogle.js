5dc41f87046cb1ad32d46e61b0fc1d55f27123d9
Mark Levison <mark@agilepainrelief.com>
2023-02-10 11:21:59 -0500

    Replace let with const

--- BooksFromGoogle.js
+++ BooksFromGoogle.js
@@ -2,8 +2,8 @@ const GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes";
 const GOOGLE_BOOKS_TITLE_TERM = "intitle:"
 
 export async function queryGoogleBookAPI(title) {
-  let encodedTitle = encodeURIComponent(GOOGLE_BOOKS_TITLE_TERM + title);
-  let finalURL = GOOGLE_BOOKS_API_URL + "?q=" + encodedTitle + "&maxResults=10";
+  const encodedTitle = encodeURIComponent(GOOGLE_BOOKS_TITLE_TERM + title);
+  const finalURL = GOOGLE_BOOKS_API_URL + "?q=" + encodedTitle + "&maxResults=10";
   // console.log(finalURL);
   const response = await fetch(finalURL);
   const data = await response.json();
